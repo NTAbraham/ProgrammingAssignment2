f560553cd1a884c6c6afee3151731a7023faaa07
Niveah <niveahte@gmail.com>
2016-05-15 14:53:19 -0400

    Last changes

--- cachematrix.R
+++ cachematrix.R
@@ -2,13 +2,18 @@
 makeCacheMatrix <- function(x = matrix())
   {
   inv <- NULL  ## initializing
-  set <- function(y) {
+  set <- function(y) 
+    {
     x <<- y #reassigning the value to a diff variable
     inv <<- NULL #initializing NULL to the variable
   }
+  
   get <- function() x
+  
   setInverse <- function(inverse) inv <<- inverse
+  
   getInverse <- function() inv
+  
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
@@ -17,7 +22,9 @@ makeCacheMatrix <- function(x = matrix())
 ##been calculated (and the matrix has not changed), 
 ##then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
+
+cacheSolve <- function(x, ...) 
+  {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
   if (!is.null(inv))  ##checking to see if it has the same value
